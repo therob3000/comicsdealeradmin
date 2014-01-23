e187432c3faa7da6e40106b4259b5cae5ad0a7dd
Carlos Mejia <carlos.mejia.rueda@gmail.com>
2014-01-22 22:09:58 -0600

    prueba heroku 6

--- php/pedidos_pendientes.php
+++ php/pedidos_pendientes.php
@@ -8,7 +8,7 @@
 
 	$json = new stdClass();
 	$cadenaColumnas = "SELECT pedido_usuario_id, pedido_id, pedido_compania_id, pedido_personaje_id, pedido_textolibre, pedido_lugar_entrega, pedido_forma_pago_id, pedido_fecha FROM pedidos WHERE pedido_estado = 0";
-	query 	= mysql_query($cadenaColumnas);
+	$query 	= mysql_query($cadenaColumnas);
 	$num 	= mysql_num_rows($query);
 	echo $num;
 
